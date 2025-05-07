554ef405a0594bcea6002dcc093fa96e4be1e882
OmwansaObed <obed9to5@gmail.com>
2025-05-07 12:11:12 +0300

    update newsletter api

--- models/newsletter.model.js
+++ models/newsletter.model.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
 const NewsLetterSchema = new mongoose.Schema({
-  email: String,
-
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
@@ -10,6 +13,6 @@ const NewsLetterSchema = new mongoose.Schema({
 });
 
 const NewsLetter =
-  mongoose.models.NewsLetter || mongoose.model("Contact", NewsLetterSchema);
+  mongoose.models.NewsLetter || mongoose.model("NewsLetter", NewsLetterSchema);
 
 export default NewsLetter;
